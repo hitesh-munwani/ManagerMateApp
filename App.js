d9abfd8824eb494d0d7f52df83acf8205baa9b60
Hitesh kumar <123413043+hitesh-munwani@users.noreply.github.com>
2024-07-17 19:12:36 +0500

    Home Screen created

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./app/LoginScreen";
+import HomeScreen from "./app/HomeScreen";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,11 @@ const App = () => {
           name="Login"
           component={LoginScreen}
           options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen} // Add HomeScreen here
+          options={{ title: "" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ app/HomeScreen.js
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.topContainer}>
+          <Image
+            source={require("../../ManagerMate/assets/DashBoard1.png")}
+            style={styles.logo}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("Attendance")}
+          >
+            <Text style={styles.buttonText}>Attendance</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("Chat")}
+          >
+            <Text style={styles.buttonText}>Chat</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("Report")}
+          >
+            <Text style={styles.buttonText}>Report</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("TaskList")}
+          >
+            <Text style={styles.buttonText}>Task List</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.bottomContainer}>
+          <Image
+            style={styles.image}
+            source={require("../../ManagerMate/assets/btmImg.png")}
+          />
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  topContainer: {
+    marginTop: 50,
+  },
+  logo: {
+    width: 150,
+    resizeMode: "center",
+    alignSelf: "center",
+  },
+  buttonContainer: {
+    width: "80%",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  button: {
+    backgroundColor: "#1E90FF",
+    padding: 15,
+    borderRadius: 5,
+    alignItems: "center",
+    marginBottom: 20,
+    width: "100%",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
+  bottomContainer: {
+    width: "100%",
+    marginTop: 30,
+  },
+  image: {
+    width: "100%",
+    height: 200, // Adjust height as needed
+  },
+});
+
+export default HomeScreen;
